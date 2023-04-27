6a99fbb6caa24bf2047a28174af6bdd02fef2633
itellida <ne8357959@gmail.com>
2023-04-27 13:23:54 +0600

    a

--- src/components/main-page/AboutUs/AboutUS.js
+++ src/components/main-page/AboutUs/AboutUS.js
@@ -4,9 +4,26 @@ import '../../../style/main/AboutUs.scss'
 
 const AboutUS = () => {
     return (
-        <div>
+        <section id="about">
+            <div className="container">
+                <div className="about">
+                    <div className="about--block">
+                        <h1 className="about--title">О НАС</h1>
+                        <div>
+                            <h2 className="about--title2">Motion Web IT academy — это международный образовательный проект, <br/>
+                                который готовит специалистов-практиков IT сферы.</h2>
+                            <h2 className="about--title2">Благодаря нашему постоянному развитию мы разработали эффективные <br/>
+                                программы для подготовки IT-специалистов. Нам удалось соединить <br/>
+                                лучшие практики офлайн — и онлайн-образования и таким образом <br/> достичь высоких результатов.</h2>
+                            <h2 className="about--title3">Подробнее</h2>
+                        </div>
+                    </div>
 
-        </div>
+
+                </div>
+            </div>
+
+        </section>  
     );
 };
 
