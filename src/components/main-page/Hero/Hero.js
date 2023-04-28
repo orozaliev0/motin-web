31269aed02fca812f12591bd162121a3a6c02b38
Kurmanbek0_0 <mr.tufio@gmail.com>
2023-04-29 05:00:23 +0600

    project and media

--- src/components/main-page/Hero/Hero.js
+++ src/components/main-page/Hero/Hero.js
@@ -5,10 +5,7 @@ import '../../../style/main/Hero.scss'
 const Hero = () => {
     return (
         <div>
-          <h1 style={{
-              color: "black"
-
-          }}>hero </h1>
+   
         </div>
     );
 };
--- src/components/main-page/Project/index.js
+++ src/components/main-page/Project/index.js
@@ -1,11 +1,38 @@
-import React from 'react';
-import '../../../style/main/Project.scss'
+import React from "react";
+import "../../../style/main/Project.scss";
+import { Link } from "react-router-dom";
+import Fly from "../../../image/main/fly-falcon.svg";
+import Sound from "../../../image/main/sound.svg";
+import Card from "../../../image/main/card.svg";
+import Pods from "../../../image/main/pods.svg";
 const Project = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+	return (
+		<div id="project">
+			<div className="container">
+				<div className="project">
+					<div className="project--photos">
+						<div className="project--photos__text">
+							<h3>Проекты студентов </h3>
+							<p>MOTIN WEB IT ACADEMY</p>
+						</div>
+						<img src={Fly} alt="" />
+						<img className="project--photos__sound" src={Sound} alt="" />
+					</div>
+					<div className="project--more">
+						<div className="project--more__img">
+							<img src={Card} alt="" />
+							<img className="project--more__img--pods" src={Pods} alt="" />
+						</div>
+						<div className="project--more__link">
+							<Link to={"/"}>
+								<span>Показать ещё</span>
+							</Link>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Project;
