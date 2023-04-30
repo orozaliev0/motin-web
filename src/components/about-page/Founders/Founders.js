4b736d0c5008844146aed7b1de000874db4af8a5
itellida <ne8357959@gmail.com>
2023-04-30 16:31:03 +0600

    us

--- src/components/about-page/Founders/Founders.js
+++ src/components/about-page/Founders/Founders.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import '../../../style/aboutUs/Founders.scss'
+import imgFound1 from "../../../image/about-us/image 6.svg"
+import imgFound2 from "../../../image/about-us/image 7.svg"
+
 const Founders = () => {
     return (
-        <div>
-            Founders About
-        </div>
+        <section id="found">
+            <div className="container">
+                <div className="found">
+                    <div>
+                        <h1 className="found--title">Основатели</h1>
+                        <p className="found--desc">Motion web IT академия была <br/>
+                            основана в апреле 2021 года <br/>
+                            по инициативе Курманбека <br/> Жоошбаева и Аскерова Канатбека</p>
+                    </div>
+                    
+                    <div className="found--block">
+                        <div>
+                            <img src={imgFound1} alt=""/>
+                            <h3 className="found--title2">Курманбек Жоошбаев</h3>
+                            <p className="found--desc2">Основатель</p>
+                        </div>
+                        <div>
+                            <img src={imgFound2} className="found--img" alt=""/>
+                            <h3 className="found--title3">Канатбек Аскеров</h3>
+                            <p className="found--desc3">Сооснователь</p>
+                        </div>
+                    </div>
+                   
+                </div>
+            </div>
+
+        </section>
     );
 };
 
--- src/components/about-page/Gallery/Gallery.js
+++ src/components/about-page/Gallery/Gallery.js
@@ -1,10 +1,57 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../../../style/aboutUs/Gallery.scss'
+import img1 from "../../../image/about-us/gallery1.svg"
+import img2 from "../../../image/about-us/gallery2.svg"
+import img3 from "../../../image/about-us/image 11.svg"
+import img4 from "../../../image/about-us/image 12.svg"
+import img5 from "../../../image/about-us/partne.svg"
+import img6 from "../../../image/about-us/Vector.svg"
+import img7 from "../../../image/about-us/partner-2svg.svg"
 const Gallery = () => {
+    const [block,setBlock] = useState(false)
     return (
-        <div>
-            Galery About
-        </div>
+        <section id="call">
+            <div className="container">
+                <div className="call">
+                    <div>
+                        <h1 className="call--title">Галерея</h1>
+                    </div>
+                    <div className="call--img">
+                        <img src={img1} alt=""/>
+                        <img src={img2} className="call--img2" alt=""/>
+                    </div>
+                </div>
+                <div className="call--box">
+                    <img src={img3} alt=""/>
+                    <img src={img4} className="call--img4" alt=""/>
+                    <span className="call--desc" onClick={() => setBlock(!block)} style={{
+                        display: block ? "none" : ''
+                    }}>Показать ещё</span>
+                </div>
+
+                {
+                    block ?  <div className="call--images">
+                        <img src={img1} alt=""/>
+                        <img src={img2} alt=""/>
+                        <img src={img3} alt=""/>
+                        <img src={img4} alt=""/>
+                        <img src={img3} alt=""/>
+                        <img src={img4} alt=""/>
+                    </div> : ''
+                }
+            </div>
+            <div className="call--boxes">
+                <div>
+                    <img src={img5} alt=""/>
+                </div>
+                <div>
+                    <img src={img6} alt=""/>
+                </div>
+                <div>
+                    <img src={img7} alt=""/>
+                </div>
+            </div>
+        </section>
     );
 };
 
