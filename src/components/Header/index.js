b6cfef4b493d71ed72059dc1ff9a0a0dab1218d8
orozaliev0 <orozaliev.pp@gmail.com>
2023-05-01 12:11:27 +0600

    7

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,12 +2,16 @@ import React, {useState} from 'react';
 import Motion from '../../image/main/LogoMotion.png'
 import language from '../../image/main/Ellipse 274.png'
 import '../../style/main/Header.scss'
+import '../../style/main/media/MediaHeader.scss'
 import {NavLink} from "react-router-dom";
 import Courses from "../Header/Courses";
+import Menu from "./Menu/Menu";
 
 const Header = () => {
 
     const [courses, setCourses] = useState(false)
+    const items = [{value : 'О нас', href : '/aboutUs' },{value : 'Frontend', href : 'frontend' },{value : 'backend', href : 'backend' },{value : 'UxUi', href : 'UxUi' },{value : 'Club', href : 'club' },{value : 'Contact' , href: '' }]
+    const [active , setActive] = useState(false)
 
     return (
         <div id='header' onMouseLeave={() => setCourses(false)}>
@@ -27,8 +31,16 @@ const Header = () => {
                         <img src={language} alt=""/>
                         <span>КР/РУС/ENG</span>
                     </div>
+                    {/*<nav className="burger_navbar">*/}
+                    {/*    <div*/}
+                    {/*        onClick={()=> setActive(!active)}*/}
+                    {/*        className="burger-btn">*/}
+                    {/*        <span/>*/}
+                    {/*    </div>*/}
+                    {/*</nav>*/}
                 </div>
             </div>
+            {/*<div className="menu--navbar"> <Menu items={items} active={active} setActive={setActive}/></div>*/}
         </div>
     );
 };
